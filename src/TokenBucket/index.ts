18973c5bfb6c459e3fb46d31906416b2e5ab49f4
Sunil <sunisimar2002d@gmail.com>
2024-03-20 18:06:14 +0530

    Add limited endpoint to TokenBucketApp

--- src/TokenBucket/index.ts
+++ src/TokenBucket/index.ts
@@ -8,6 +8,10 @@ TokenBucketApp.get('/unlimited', (req,res)=>{
     res.send("Unlimited! let's start");
 })
 
+TokenBucketApp.get('/limited',(req,res)=>{
+    res.send("Limited! don't overuse me")
+})
+
 TokenBucketApp.listen(PORT, ()=>{
     console.log(`Server is running at http://localhost:${PORT}`);
 })
